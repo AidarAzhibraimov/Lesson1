58a1aa93c8ff6fab95d8b186de3a37b67a39737d
Aidar <aidar.azhibraimov@mail.ru>
2021-11-09 21:11:59 +0600

    changed the status bar

--- app/src/main/java/com/geektech/lesson1/MainActivity.java
+++ app/src/main/java/com/geektech/lesson1/MainActivity.java
@@ -1,10 +1,12 @@
 package com.geektech.lesson1;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,12 +24,15 @@ public class MainActivity extends AppCompatActivity {
     private ImageView image;
     private TextInputEditText username, password;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         setContentView(R.layout.activity_main);
         image = findViewById(R.id.imageView);
         String URI = "https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg";
         Glide.with(this).load(URI).into(image);
     }
+
 }
